ea43a741167ea5dbcb55b6a7204179f09dde577f
ahmet <ahmetozlu93@gmail.com>
2022-11-20 17:17:27 +0300

    develop decentralized bank main class

---
+++ contracts/DecentralizedBank.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+contract DecentralizedBank {  
+
+  mapping(address => uint) public depositStart;
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public collateralEther;
+
+  mapping(address => bool) public isDeposited;
+  mapping(address => bool) public isBorrowed;
+
+  function deposit() payable public {
+    require(isDeposited[msg.sender] == false, 'Error, deposit already active');
+    require(msg.value>=1e16, 'Error, deposit must be >= 0.01 ETH');
+
+    etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+
+    isDeposited[msg.sender] = true; //activate deposit status  
+  }
+
+  function withdraw() public {
+    require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
+    uint userBalance = etherBalanceOf[msg.sender]; //for event
+
+    //check user's hodl time
+    uint depositTime = block.timestamp - depositStart[msg.sender];
+
+    //31668017 - interest(10% APY) per second for min. deposit amount (0.01 ETH), cuz:
+    //1e15(10% of 0.01 ETH) / 31577600 (seconds in 365.25 days)
+
+    //(etherBalanceOf[msg.sender] / 1e16) - calc. how much higher interest will be (based on deposit), e.g.:
+    //for min. deposit (0.01 ETH), (etherBalanceOf[msg.sender] / 1e16) = 1 (the same, 31668017/s)
+    //for deposit 0.02 ETH, (etherBalanceOf[msg.sender] / 1e16) = 2 (doubled, (2*31668017)/s)
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
+
+    //send funds to user
+    msg.sender.transfer(etherBalanceOf[msg.sender]); //eth back to user
+    //ERC20 token integration will be developed for -> interest to user
+
+    //reset depositer data
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;    
+  }
+
+  function borrow() payable public {
+    require(msg.value>=1e16, 'Error, collateral must be >= 0.01 ETH');
+    require(isBorrowed[msg.sender] == false, 'Error, loan already taken');
+
+    //this Ether will be locked till user payOff the loan
+    collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
+
+    //calc tokens amount to mint, 50% of msg.value
+    uint tokensToMint = collateralEther[msg.sender] / 2;
+
+    //mint&send tokens to user
+    //ERC20 integration will be developed
+
+    //activate borrower's loan status
+    isBorrowed[msg.sender] = true;    
+  }
+
+  function payOff() public {
+    require(isBorrowed[msg.sender] == true, 'Error, loan not active');    
+
+    uint fee = collateralEther[msg.sender]/10; //calc 10% fee
+
+    //send user's collateral minus fee
+    msg.sender.transfer(collateralEther[msg.sender]-fee);
+
+    //reset borrower's data
+    collateralEther[msg.sender] = 0;
+    isBorrowed[msg.sender] = false;    
+  }
+}
+
